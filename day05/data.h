68ee3451463dfe065e4c1c458fff3c16e679678e
FluffyGameDev <71411509+FluffyGameDev@users.noreply.github.com>
2023-12-05 13:49:04 +0100

    Day 5 part 1 solution

---
+++ day05/data.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <vector>
+#include "types.h"
+
+namespace AoC
+{
+    const char* s_testInputData
+    {
+        "seeds: 79 14 55 13\n"
+        "\n"
+        "seed-to-soil map:\n"
+        "50 98 2\n"
+        "52 50 48\n"
+        "\n"
+        "soil-to-fertilizer map:\n"
+        "0 15 37\n"
+        "37 52 2\n"
+        "39 0 15\n"
+        "\n"
+        "fertilizer-to-water map:\n"
+        "49 53 8\n"
+        "0 11 42\n"
+        "42 0 7\n"
+        "57 7 4\n"
+        "\n"
+        "water-to-light map:\n"
+        "88 18 7\n"
+        "18 25 70\n"
+        "\n"
+        "light-to-temperature map:\n"
+        "45 77 23\n"
+        "81 45 19\n"
+        "68 64 13\n"
+        "\n"
+        "temperature-to-humidity map:\n"
+        "0 69 1\n"
+        "1 0 69\n"
+        "\n"
+        "humidity-to-location map:\n"
+        "60 56 37\n"
+        "56 93 4"
+    };
+
+    struct ConversionRange
+    {
+        u64 DestinationRangeStart;
+        u64 SourceRangeStart;
+        u64 RangeLength;
+    };
+
+    struct ConversionMap
+    {
+        std::vector<ConversionRange> Ranges;
+    };
+
+    struct InputData
+    {
+        std::vector<u64> Seeds;
+        std::vector<ConversionMap> ConversionMaps;
+    };
+
+    struct OutputData
+    {
+        u64 LowestLocationNumber;
+        u64 LowestLocationNumberRanges;
+    };
+}
---
+++ day05/solution.cpp
@@ -0,0 +1,98 @@
+﻿#include "data.h"
+#include "aoc.h"
+
+#include <numeric>
+
+namespace AoC
+{
+    namespace Internal
+    {
+        bool IsInRange(u64 element, const ConversionRange& range)
+        {
+            return (element >= range.SourceRangeStart && element < range.SourceRangeStart + range.RangeLength);
+        }
+
+        u64 ConvertElement(u64 element, const ConversionMap& conversionMap)
+        {
+            for (const auto& range : conversionMap.Ranges)
+            {
+                if (IsInRange(element, range))
+                {
+                    return element - range.SourceRangeStart + range.DestinationRangeStart;
+                }
+            }
+
+            return element;
+        }
+    }
+
+    bool ReadInput(std::istream& inputStream, InputData& inputData, AoCStep step)
+    {
+        std::string ignore{};
+        std::string line{};
+        if (std::getline(inputStream, line))
+        {
+            std::stringstream lineStream{ line };
+            lineStream >> ignore;
+
+            u64 seed{};
+            while (lineStream >> seed)
+            {
+                inputData.Seeds.push_back(seed);
+            }
+
+            std::getline(inputStream, line);
+        }
+        else
+        {
+            return false;
+        }
+
+        while (std::getline(inputStream, line))
+        {
+            ConversionMap& newConversionMap{ inputData.ConversionMaps.emplace_back() };
+            while (std::getline(inputStream, line) && !line.empty())
+            {
+                ConversionRange& newRange{ newConversionMap.Ranges.emplace_back() };
+                std::stringstream lineStream{ line };
+                lineStream >> newRange.DestinationRangeStart >> newRange.SourceRangeStart >> newRange.RangeLength;
+            }
+        }
+
+        return true;
+    }
+
+    void ComputeOutput(const InputData& input, OutputData& output)
+    {
+        std::vector<u64> currentStep{ input.Seeds };
+        for (const auto& conversionMap : input.ConversionMaps)
+        {
+            std::transform(currentStep.begin(), currentStep.end(), currentStep.begin(),
+                [&conversionMap](u64 element) { return Internal::ConvertElement(element, conversionMap); });
+        }
+
+        output.LowestLocationNumber = std::accumulate(currentStep.begin(), currentStep.end(), std::numeric_limits<u64>::max(),
+            [](u64 currentMin, u64 element) { return std::min(currentMin, element); });
+    }
+
+    bool ValidateTestOutput(const OutputData& output)
+    {
+        bool didTestsPass{ true };
+
+        didTestsPass &= output.LowestLocationNumber == 35;
+        didTestsPass &= output.LowestLocationNumberRanges == 46;
+
+        return didTestsPass;
+    }
+
+    void PrintOutput(const OutputData& output)
+    {
+        fmt::print("Lowest Location Number: {}\n", output.LowestLocationNumber);
+        fmt::print("Lowest Location Number with Ranges: {}\n", output.LowestLocationNumberRanges);
+    }
+}
+
+void main()
+{
+    AoC::Run<AoC::InputData, AoC::OutputData>(AoC::s_testInputData);
+}
